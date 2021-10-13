674ce8be60a42d2dfe09f1a90834517d5957ffa9
Alessandro Elizondo <alessandro.uanl@gmail.com>
2021-10-12 20:00:21 -0500

    Update
    Update

--- src/app/publicacion/publicacion.component.ts
+++ src/app/publicacion/publicacion.component.ts
@@ -1,8 +1,5 @@
-import { IonicModule } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, ParamMap } from '@angular/router';
-import { Location } from '@angular/common';
-import * as data from '../../assets/feed.json';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-publicacion',
@@ -11,52 +8,17 @@ import * as data from '../../assets/feed.json';
 })
 export class PublicacionComponent implements OnInit {
 
-  _filtrarPublicacion: number; //filtro de publicaciones en el perfil
-
-  publicacion: any;
-
-  usuario: any;
-
-  datos: any = data;
-
-  publicaciones: any = this.datos.publicaciones;
-
-  detallePublicacion: any;
-
-
-  get filtrarPublicacion(): number {
-    return this._filtrarPublicacion;
-  }
-
-  set filtrarPublicacion(valor: number) {
-    console.log(valor);
-   
-    this._filtrarPublicacion = valor;
-
-
-    this.detallePublicacion = this.cargarDetallePublicacion(valor);
-
-   
-  }
-
-  cargarDetallePublicacion(idPublicacion: number):any {
-     return this.publicaciones.find((publicacion: any) => publicacion.id );
-  }
-  volver(): void {
-    this._location.back();
-  }
-  constructor(private RutaActiva: ActivatedRoute, private _location : Location){}
+  idPublicacion: number;
   
+  constructor(private RutaActiva: ActivatedRoute) { }
+
   ngOnInit() {
 
-    
+    this.idPublicacion = this.RutaActiva.snapshot.params.publicacionId;
+    console.log(this.idPublicacion);
 
-    this._filtrarPublicacion = this.RutaActiva.snapshot.params.publicacionId;
-    console.log(this._filtrarPublicacion);
-
-
-    this.detallePublicacion =  this.cargarDetallePublicacion(this._filtrarPublicacion);
-    
+    //this.idPublicacion = this.RutaActiva.snapshot.params.id;
+    //console.log(this.idPublicacion);
   }
 
 }
--- src/app/publicaciones/publicaciones.component.ts
+++ src/app/publicaciones/publicaciones.component.ts
@@ -1,35 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { PublicacionRoutingModule } from '../publicacion/publicacion-routing.module';
+import { HttpClient } from '@angular/common/http';
+import * as data from '../feed.json';
+import { dashCaseToCamelCase } from '@angular/compiler/src/util';
+
 
 export interface Publicaciones{
   imagen: String;
   id: number;
 }
+
+
 @Component({
   selector: 'app-publicaciones',
   templateUrl: './publicaciones.component.html',
   styleUrls: ['./publicaciones.component.scss'],
 })
 export class PublicacionesComponent implements OnInit {
+
   
-  constructor() { }
-  
-  ngOnInit() {}
-  
-  
-  publicacionesArreglo: Publicaciones[] = [
-    {
-      imagen: '../assets/images/Post 1.jpg',
-      id: 1
-    },
-    {
-      imagen: '../assets/images/Post 2.png',
-      id: 3
-    },
-    {
-      imagen: '../assets/images/Post 3.png',
-      id: 2
-    }
-  ]
-  
+  constructor(private http: HttpClient) { }
+
+
+  publicacionesArreglo: Publicaciones[] = [];
+  //publicaciones: {id: number, usuario: String, imagenPost: String, avatarUsuario: string,  descripcionPost: String}[] = publicaciones;
+   datos =  data;
+   datosUsuario: any = this.datos.usuario;
+   publicaciones: any = this.datos.publicaciones;
+   _filtrarPublicaciones: string = this.datosUsuario.nombre; //filtro de publicaciones en el perfil
+   publicacionesPorUsuario = [];
+
+   get filtrarPublicaciones(): string {
+     return this._filtrarPublicaciones;
+   }
+   set filtrarPublicaciones(valor: string) {
+     console.log(valor);
+     this._filtrarPublicaciones = valor;
+     this.publicacionesPorUsuario = this.filtroPublicaciones(valor);
+   }
+
+   filtroPublicaciones(nombreUsuario: String): []{
+     nombreUsuario = nombreUsuario.toLocaleLowerCase();
+     return this.publicaciones.filter((publicacion: any) => publicacion.usuario.toLocaleLowerCase().includes(nombreUsuario));
+   }
+
+  ngOnInit() {
+    this.filtrarPublicaciones = this.datosUsuario.nombre;
+  }
+
+
 }
